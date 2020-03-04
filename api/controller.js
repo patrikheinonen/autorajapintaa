1672120d0a7fc340489df40d2460a2300186d4bf
patrikheinonen <patriksh@metropolia.fi>
2020-03-04 18:03:27 +0200

    added functionality to buttons etc, fixed get

--- api/controller.js
+++ api/controller.js
@@ -9,32 +9,33 @@ var bodyParser = require("body-parser");
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({
-    extended: false }));
+    extended: false
+}));
 app.use(bodyParser.json());
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "ryhma8password",
+    password: "root",
     database: "car_database"
 });
 
 const query = util.promisify(con.query).bind(con);
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
 });
 //tarvitset tätä perkele!
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(process.cwd(), '/views/index.html'));
+    console.log(process.cwd())
 });
 
-app.get("/cars", function(req,res){
+app.get("/cars", function (req, res) {
     var sql = "SELECT * FROM auto WHERE Malli = ?";
     var q = url.parse(req.url, true).query;
     var malli = q.name;
-
-    if (malli.length === 0) {
+    if (malli == null || malli.length === 0) {
         var sql = "SELECT * FROM auto";
     } else {
         var sql = "SELECT * FROM auto WHERE Malli = ?";
@@ -47,62 +48,62 @@ app.get("/cars", function(req,res){
             } else {
                 res.send(json);
             }
-        } catch(err) {
+        } catch (err) {
             console.log("Database Error!");
         }
     })();
 });
 
-    app.post("/cars", function (req, res) {
+app.post("/cars", function (req, res) {
 
-        var sql = "INSERT INTO Auto VALUES (?, ?, ?, ?, ?, ?)";
-        var mark = req.body.Merkki;
-        var model = req.body.Malli;
-        var year = req.body.Valmistusvuosi;
-        var fuel = req.body.Polttoaine;
-        var weight = req.body.Paino;
-        var co2 = req.body.CO2;
-        var price = req.body.Hinta;
-        var topSpeed = req.body.MaxNopeus;
-        var from0to100 = req.body.NollastaSataan;
-        var horsePower = req.body.HevosVoimat;
-        var wheels = req.body.VetävätRenkaat;
+    var sql = "INSERT INTO Auto VALUES (?, ?, ?, ?, ?, ?)";
+    var mark = req.body.Merkki;
+    var model = req.body.Malli;
+    var year = req.body.Valmistusvuosi;
+    var fuel = req.body.Polttoaine;
+    var weight = req.body.Paino;
+    var co2 = req.body.CO2;
+    var price = req.body.Hinta;
+    var topSpeed = req.body.MaxNopeus;
+    var from0to100 = req.body.NollastaSataan;
+    var horsePower = req.body.HevosVoimat;
+    var wheels = req.body.VetävätRenkaat;
 
-        (async () => {
-            try {
-                const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
-                res.send(json);
-            } catch(err) {
-                console.log("Database Error!");
-            }
-        })();
-    });
+    (async () => {
+        try {
+            const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
+            res.send(json);
+        } catch (err) {
+            console.log("Database Error!");
+        }
+    })();
+});
 
-    app.put("/cars", function (req, res) {
+app.put("/cars", function (req, res) {
 
-        var sql = "UPDATE Auto SET Merkki = ?, Malli = ?, Valmistusvuosi = ?, Polttoaine = ?, Paino = ?, CO2 = ?, Hinta = ?, MaxNopeus = ?, NollastaSataan = ?, HevosVoimat = ?, VetävätRenkaat = ? WHERE AutoID = ?";
-        var id = req.body.AutoID;
-        var mark = req.body.Merkki;
-        var model = req.body.Malli;
-        var year = req.body.Valmistusvuosi;
-        var fuel = req.body.Polttoaine;
-        var weight = req.body.Paino;
-        var co2 = req.body.CO2;
-        var price = req.body.Hinta;
-        var topSpeed = req.body.MaxNopeus;
-        var from0to100 = req.body.NollastaSataan;
-        var horsePower = req.body.HevosVoimat;
-        var wheels = req.body.VetävätRenkaat;
+    var sql = "UPDATE Auto SET Merkki = ?, Malli = ?, Valmistusvuosi = ?, Polttoaine = ?, Paino = ?, CO2 = ?, Hinta = ?, MaxNopeus = ?, NollastaSataan = ?, HevosVoimat = ?, VetävätRenkaat = ? WHERE AutoID = ?";
+    var id = req.body.AutoID;
+    var mark = req.body.Merkki;
+    var model = req.body.Malli;
+    var year = req.body.Valmistusvuosi;
+    var fuel = req.body.Polttoaine;
+    var weight = req.body.Paino;
+    var co2 = req.body.CO2;
+    var price = req.body.Hinta;
+    var topSpeed = req.body.MaxNopeus;
+    var from0to100 = req.body.NollastaSataan;
+    var horsePower = req.body.HevosVoimat;
+    var wheels = req.body.VetävätRenkaat;
 
-        (async () => {
-            try {
-                const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
-                res.send(json);
-            } catch(err) {
-                console.log("Database Error!");
-            }
-        })();
-    });
+    (async () => {
+        try {
+            const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
+            res.send(json);
+        } catch (err) {
+            console.log("Database Error!");
+        }
+    })();
+});
 
 try {
     app.listen(8082, function () {
@@ -110,4 +111,5 @@ try {
     });
 } catch {
 
-};
+}
+;
--- public/javascripts/cars.js
+++ public/javascripts/cars.js
@@ -1,18 +1,19 @@
 function searchCar() {
     var name = document.getElementById("car").value;
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             try {
                 var json = JSON.parse(xmlhttp.responseText);
-                if (json.length> 0){ // something found
+                console.log(json)
+                if (json.length > 0) { // something found
                     console.table(json);
                     showList(json);
-                }
-                else {
+                } else {
                     console.log(json)
                 }
             } catch {
+
                 document.getElementById("row").innerHTML = "Haulla ei löytynyt mitään"
 
             }
@@ -21,17 +22,56 @@ function searchCar() {
         }
     };
     console.log(name);
-    xmlhttp.open("GET", "http://localhost:8082/cars?name="+ name, true);
+    xmlhttp.open("GET", "http://localhost:8082/cars?name=" + name, true);
     xmlhttp.send();
 }
 
+function addFunctionalityToButtons(){
+    var buttons = document.getElementsByName("deletebtn");
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function () {
+            var id = parseInt(this.parentElement.parentElement.firstChild.innerHTML);
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    try {
+                        var json = JSON.parse(xmlhttp.responseText);
+                        console.log(json)
+                        if (json.length > 0) { // something found
+                            console.table(json);
+                            showList(json);
+                        } else {
+                            console.log(json)
+                        }
+                    } catch {
+
+                        document.getElementById("row").innerHTML = "Haulla ei löytynyt mitään"
+
+                    }
+
+
+                }
+            };
+            console.log(name);
+            xmlhttp.open("GET", "http://localhost:8082/cars?name=" + name, true);
+            xmlhttp.send();
+
+        }
+
+    }
+
+
+}
 
 function showList(json) {
     var locationInfo = document.getElementById("locationInfo");
+
     locationInfo.innerHTML =
         "<table>" +
         "<tr>" +
-        "<th>Merkki</th>"+
+        "<th>ID</th>" +
+        "<th>Merkki</th>" +
         "<th>Malli</th>" +
         "<th>Valmistusvuosi</th>" +
         "<th>Polttoaine</th>" +
@@ -42,20 +82,25 @@ function showList(json) {
         "<th>Nollasta sataan</th>" +
         "<th>Hevosvoimat</th>" +
         "<th>Vetävätrenkaat</th>" +
+        "<th>Poista </th>" +
         "</tr>" +
         "</table>";
-    for(var i=0; i<json.length; i++) {
+
+    for (var i = 0; i < json.length; i++) {
         var row = locationInfo.firstChild.insertRow(-1);
-        row.insertCell(0).innerHTML = (json[i].Merkki);
-        row.insertCell(1).innerHTML = (json[i].Malli);
-        row.insertCell(2).innerHTML = (json[i].Valmistusvuosi);
-        row.insertCell(3).innerHTML = (json[i].Polttoaine);
-        row.insertCell(4).innerHTML = (json[i].Paino);
-        row.insertCell(5).innerHTML = (json[i].CO2);
-        row.insertCell(6).innerHTML = (json[i].Hinta);
-        row.insertCell(7).innerHTML = (json[i].MaxNopeus);
-        row.insertCell(8).innerHTML = (json[i].NollastaSataan);
-        row.insertCell(9).innerHTML = (json[i].HevosVoimat);
-        row.insertCell(10).innerHTML = (json[i].VetävätRenkaat);
+        row.insertCell(0).innerHTML = (json[i].AutoId);
+        row.insertCell(1).innerHTML = (json[i].Merkki);
+        row.insertCell(2).innerHTML = (json[i].Malli);
+        row.insertCell(3).innerHTML = (json[i].Valmistusvuosi);
+        row.insertCell(4).innerHTML = (json[i].Polttoaine);
+        row.insertCell(5).innerHTML = (json[i].Paino);
+        row.insertCell(6).innerHTML = (json[i].CO2);
+        row.insertCell(7).innerHTML = (json[i].Hinta);
+        row.insertCell(8).innerHTML = (json[i].MaxNopeus);
+        row.insertCell(9).innerHTML = (json[i].NollastaSataan);
+        row.insertCell(10).innerHTML = (json[i].HevosVoimat);
+        row.insertCell(11).innerHTML = (json[i].VetävätRenkaat);
+        row.insertCell(12).innerHTML = "<button type='button' name='deletebtn'>Poista</button>";
     }
+    addFunctionalityToButtons();
 }
