18765a24e620447e40ee58808748b2b00e5e7693
joonatha <joonatan.hamalainen@metropolia.fi>
2020-03-05 16:51:18 +0200

    working put and post for controller.js

--- api/controller.js
+++ api/controller.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "ryhma8password",
     database: "car_database"
 });
 
@@ -52,55 +52,70 @@ app.get("/cars", async function (req, res) {
 
 });
 
+function post(req, res) {
+    var sql = "INSERT INTO Auto VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    var mark = req.body.Merkki;
+    var model = req.body.Malli;
+    var year = req.body.Valmistusvuosi;
+    var fuel = req.body.Polttoaine;
+    var weight = req.body.Paino;
+    var co2 = req.body.CO2;
+    var price = req.body.Hinta;
+    var topSpeed = req.body.MaxNopeus;
+    var from0to100 = req.body.NollastaSataan;
+    var horsePower = req.body.HevosVoimat;
+    var wheels = req.body.VetävätRenkaat;
+
+    (async () => {
+        try {
+            const json = await query(sql, [null, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
+            res.send(json);
+        } catch (err) {
+            console.log("Database Error!");
+        }
+    })();
+}
+
 app.post("/cars", function (req, res) {
-
-    var sql = "INSERT INTO Auto VALUES (?, ?, ?, ?, ?, ?)";
-    var mark = req.body.Merkki;
-    var model = req.body.Malli;
-    var year = req.body.Valmistusvuosi;
-    var fuel = req.body.Polttoaine;
-    var weight = req.body.Paino;
-    var co2 = req.body.CO2;
-    var price = req.body.Hinta;
-    var topSpeed = req.body.MaxNopeus;
-    var from0to100 = req.body.NollastaSataan;
-    var horsePower = req.body.HevosVoimat;
-    var wheels = req.body.VetävätRenkaat;
-
-    (async () => {
-        try {
-            const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
-            res.send(json);
-        } catch (err) {
-            console.log("Database Error!");
-        }
-    })();
+    post(req, res);
 });
 
-app.put("/cars", function (req, res) {
 
-    var sql = "UPDATE Auto SET Merkki = ?, Malli = ?, Valmistusvuosi = ?, Polttoaine = ?, Paino = ?, CO2 = ?, Hinta = ?, MaxNopeus = ?, NollastaSataan = ?, HevosVoimat = ?, VetävätRenkaat = ? WHERE AutoID = ?";
-    var id = req.body.AutoID;
-    var mark = req.body.Merkki;
-    var model = req.body.Malli;
-    var year = req.body.Valmistusvuosi;
-    var fuel = req.body.Polttoaine;
-    var weight = req.body.Paino;
-    var co2 = req.body.CO2;
-    var price = req.body.Hinta;
-    var topSpeed = req.body.MaxNopeus;
-    var from0to100 = req.body.NollastaSataan;
-    var horsePower = req.body.HevosVoimat;
-    var wheels = req.body.VetävätRenkaat;
-    (async () => {
-        try {
-            const json = await query(sql, [id, mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels]);
-            res.send(json);
-        } catch (err) {
-            console.log("Database Error!");
-        }
-    })();
-});
+app.put("/cars", async function (req, res) {
+    var q = url.parse(req.url, true).query;
+    var id = q.id;
+    var sql = "SELECT * FROM auto WHERE AutoID = ?";
+try {
+    const json = await query(sql, [id]);
+    if (json.length === 0) {
+        post(req, res);
+    } else {
+        var sql = "UPDATE Auto SET Merkki = ?, Malli = ?, Valmistusvuosi = ?, Polttoaine = ?, Paino = ?, CO2 = ?, Hinta = ?, MaxNopeus = ?, NollastaSataan = ?, HevosVoimat = ?, VetävätRenkaat = ? WHERE AutoID = ?";
+        var mark = req.body.Merkki;
+        var model = req.body.Malli;
+        var year = req.body.Valmistusvuosi;
+        var fuel = req.body.Polttoaine;
+        var weight = req.body.Paino;
+        var co2 = req.body.CO2;
+        var price = req.body.Hinta;
+        var topSpeed = req.body.MaxNopeus;
+        var from0to100 = req.body.NollastaSataan;
+        var horsePower = req.body.HevosVoimat;
+        var wheels = req.body.VetävätRenkaat;
+        (async () => {
+            try {
+                const json = await query(sql, [mark, model, year, fuel, weight, co2, price, topSpeed, from0to100, horsePower, wheels, id]);
+                res.send(json);
+            } catch (err) {
+                console.log("Database Error!");
+            }
+        })();
+    }
+} catch (err) {
+    console.log("Database Error!");
+}
+})
+
 
 app.delete("/cars", function (req, res) {
     var q = url.parse(req.url, true).query;
